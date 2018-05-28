f420c32702f9ba7120fc0a5c4e218741410e4294
Djanusk <d.janusk@gmail.com>
2018-05-28 02:15:45 +0200

    Add All Files

--- chest/controllers/menu-controller.js
+++ chest/controllers/menu-controller.js
@@ -1,12 +1,28 @@
 app.controller("menuCtrl", function($scope, $location) {
 
+    $scope.login = function() {
+        $scope.myLogin = true;
+    }
+
+    $scope.logout = function() {
+        $scope.myLogin = false;
+    }
+
+    $scope.register = function() {
+        $scope.myReg = true;
+    }
+
+    $scope.registered = function() {
+        $scope.myReg = false;
+    }
+
     $scope.getClass = function(path) {
         if(path == "/home" && $location.path() == "/") return "active";
         return ($location.path() === path) ? "active" : "";
     }
 })
 
-    app.run(['$rootScope', function($rootScope) {
+app.run(['$rootScope', function($rootScope) {
         $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
             $rootScope.title = current.$$route.title;
     });
